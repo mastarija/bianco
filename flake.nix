ae635d09a156dd0d93468d07ea4f907a1e0a7dde
mastarija <luka@mastarija.com>
2025-04-02 21:34:11 +0200

    light refactor

--- flake.nix
+++ flake.nix
@@ -4,30 +4,34 @@
   inputs =
   {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self , nixpkgs }:
-  {
-    devShells.x86_64-linux.default = with import nixpkgs { system = "x86_64-linux"; }; with pkgs;
-    let
-      ghc = haskell.compiler.ghc9101;
-      ghc-js = haskell.compiler.ghc9101.override
-      {
-        stdenv = stdenv.override { targetPlatform = pkgsCross.ghcjs.stdenv.targetPlatform; };
-      };
-    in
-      mkShell
-      {
-        packages = [ ghc ghc-js cabal-install ghcid emscripten ];
-        shellHook = ''
-          alias ghc-js=javascript-unknown-ghcjs-ghc
+  outputs = { self , nixpkgs , flake-utils }: flake-utils.lib.eachDefaultSystem
+  ( system :
+    {
+      devShells.default = with import nixpkgs { inherit system; }; with pkgs;
+      let
+        ghc-nt = haskell.compiler.ghc912;
+        ghc-js = haskell.compiler.ghc912.override
+        {
+          stdenv = stdenv.override { targetPlatform = pkgsCross.ghcjs.stdenv.targetPlatform; };
+        };
+      in
+        mkShell
+        {
+          packages = [ ghc-nt ghc-js cabal-install ghcid emscripten ];
+          shellHook =
+          ''
+            if [ ! -d $(pwd)/.emscripten_cache ]; then
+              cp -R ${emscripten}/share/emscripten/cache/ $(pwd)/.emscripten_cache
+              chmod u+rwX -R $(pwd)/.emscripten_cache
+              export EM_CACHE=$(pwd)/.emscripten_cache
+            fi
 
-          if [ ! -d $(pwd)/.emscripten_cache ]; then
-            cp -R ${emscripten}/share/emscripten/cache/ $(pwd)/.emscripten_cache
-            chmod u+rwX -R $(pwd)/.emscripten_cache
-            export EM_CACHE=$(pwd)/.emscripten_cache
-          fi
-        '';
-      };
-  };
+            alias ghc-js=javascript-unknown-ghcjs-ghc
+          '';
+        };
+    }
+  );
 }
