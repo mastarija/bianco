b87a9a405107ce08b897a6e2a8b3dc71d664e488
mastarija <luka@mastarija.com>
2025-04-02 10:24:22 +0200

    working ghc javascript backend

--- flake.nix
+++ flake.nix
@@ -3,15 +3,31 @@
 
   inputs =
   {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    ghcjs.url = "git+https://gitlab.haskell.org/ghc/ghc.nix#js-cross";
+    nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self , nixpkgs , ghcjs }:
+  outputs = { self , nixpkgs }:
   {
-    devShells.x86_64-linux.default = with import nixpkgs { system = "x86_64-linux"; }; pkgs.mkShell
-    {
-      packages = with pkgs; [ haskell.compiler.ghc910 cabal-install ghcid ghcjs ];
-    };
+    devShells.x86_64-linux.default = with import nixpkgs { system = "x86_64-linux"; }; with pkgs;
+    let
+      ghc = haskell.compiler.ghc9101;
+      ghc-js = haskell.compiler.ghc9101.override
+      {
+        stdenv = stdenv.override { targetPlatform = pkgsCross.ghcjs.stdenv.targetPlatform; };
+      };
+    in
+      mkShell
+      {
+        packages = [ ghc ghc-js cabal-install ghcid emscripten ];
+        shellHook = ''
+          alias ghc-js=javascript-unknown-ghcjs-ghc
+
+          if [ ! -d $(pwd)/.emscripten_cache ]; then
+            cp -R ${emscripten}/share/emscripten/cache/ $(pwd)/.emscripten_cache
+            chmod u+rwX -R $(pwd)/.emscripten_cache
+            export EM_CACHE=$(pwd)/.emscripten_cache
+          fi
+        '';
+      };
   };
 }
